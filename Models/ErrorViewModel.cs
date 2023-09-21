5a1c5a926fc1978ee8e46580d137385c1bf9ead4
Guilbault2063074 <81373503+Guilbault2063074@users.noreply.github.com>
2023-09-21 19:57:20 -0400

    First push

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projet_Web_Serveur.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
